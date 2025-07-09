00a70a142197cceb2b97ea1212e5faeb4b164a89
jpzex <joao.pedro.menezes1998@GMAIL.COM>
2025-07-09 18:44:36 -0300

    Update base_opt.sh to v1.0.3_test5 (old)
    Old version, do not use.

--- base_opt.sh
+++ base_opt.sh
@@ -1,24 +1,22 @@
 # base_opt.sh
-# Version 1.0.2
-# 2021-06-22 @ 23:30 (UTC)
-# ID: 2wnukx
+# Version 1.0.3
+# 2022-05-23 @ 10:42 (UTC)
+# ID: test5
 # Written by jpzex@XDA
 # Use at your own risk, Busybox is required.
 
-which busybox > /dev/null || $(echo "No busybox found." && exit 0)
+which busybox > /dev/null
 
-alias_list="
-mountpoint
-awk
-echo
-grep
-chmod
-fstrim
-cat
-mount"
+if [ ! $? == 0 ]; then
+    echo "No busybox found.";
+    exit 0;
+fi
+
+alias_list="mountpoint awk echo grep chmod fstrim cat mount"
 
 for x in $alias_list; do
-alias $x="busybox $x"; done
+    alias $x="busybox $x";
+done
 
 scriptname=base_opt
 dumpE=0
@@ -28,7 +26,7 @@ dumpE=0
 
 generic_opt(){
 M1 # ext4 and f2fs mountpoints
-#M2 # sysctl (generic)
+M2 # sysctl (generic)
 M4 # I/O
 }
 
@@ -44,52 +42,36 @@ read /proc/mounts | grep " / ext4" > /dev/null && root=/
 
 list="/system /data /cache /data/sdext2 $root"
 
-for x in $list; do [ -e $x ] && [ ! -L $x ] && mountpoint -q $x && search " $x " /proc/mounts && list2="$x
-$list2"
-
-devlist="$(grep /dev/block /proc/mounts | grep "$x " | awk '{ print $1 }')
-$devlist"; done
-
 for x in $list; do
-
-case $(grep " $x " /proc/mounts | awk '{ print $3 }') in
-
-"ext4")
-flags="
-discard
-barrier
-noatime
-nodiratime
-";;
-
-# noblock_validity,inode_readahead_blks=16
-# auto_da_alloc,commit=10,delalloc
-# max_batch_time=100000
-
-"f2fs")
-flags="
-active_logs=2
-noatime
-nodiratime
-noacl
-";;
-
-# flush_merge,disable_ext_identify,nobarrier
-# nouser_xattr,no_heap,nodiscard - error when apply
-
-*)
-break;; 
-
-esac
-
-if [ $dumpE == 0 ]; then
-for y in $flags; do mount -o remount,$y $x; done; fi
-
+    [ -e $x ] && [ ! -L $x ] && mountpoint -q $x &&\
+    search " $x " /proc/mounts && list2="$x $list2"
+    devlist="$(grep /dev/block /proc/mounts | \
+    grep "$x " | awk '{ print $1 }') $devlist";
 done
 
-# fstrim all f2fs and ext4 partitions found.
+for x in $list; do
+    case $(grep " $x " /proc/mounts | awk '{ print $3 }') in
+        "ext4")
+            flags="discard barrier noatime nodiratime";;
+        "f2fs")
+            flags="active_logs=4 noatime nodiratime \
+            noacl nobarrier background_gc=on";;
+         *)
+            break;; 
+    esac
+    if [ $dumpE == 0 ]; then
+        for y in $flags; do
+            mount -o remount,$y $x
+        done
+    fi
+done
 
-[ $(which fstrim) ] && [ $dumpE == 0 ] && for x in $list2; do [ -e $x ] && mountpoint -q $x && fstrim $x; done
+# fstrim all existing f2fs and ext4 partitions.
+
+[ $(which fstrim) ] && [ $dumpE == 0 ] &&\
+for x in $list2; do
+    [ -e $x ] && mountpoint -q $x && fstrim $x
+done
 
 unset list list2 devlist flags x root
 
@@ -105,27 +87,25 @@ M2(){
 
 sys=/proc/sys
 
-fs_generic(){
+# fs_base
 wr $sys/fs/file-max 131072
 wr $sys/fs/inotify/max_queued_events 1048576
 #wr $sys/fs/inotify/max_user_instances 1024 # not mess
 wr $sys/fs/inotify/max_user_watches 1048576
-}
 
-kernel_generic(){
+# kernel_base
 wr $sys/kernel/ctrl-alt-del 0
 wr $sys/kernel/panic 10
 wr $sys/kernel/panic_on_oops 1
 wr $sys/kernel/printk "0 0 0 0"
-}
 
-net_generic(){
-wr $sys/net/core/netdev_max_backlog 256
-wr $sys/net/core/rmem_default 262144
-wr $sys/net/core/rmem_max 262144
-wr $sys/net/core/somaxconn 32
-wr $sys/net/core/wmem_default 262144
-wr $sys/net/core/wmem_max 262144
+# net_base
+wr $sys/net/core/netdev_max_backlog 512
+wr $sys/net/core/rmem_default 524288
+wr $sys/net/core/rmem_max 4194304
+wr $sys/net/core/somaxconn 512
+wr $sys/net/core/wmem_default 524288
+wr $sys/net/core/wmem_max 4194304
 wr $sys/net/ipv4/ipfrag_high_thresh 8388608
 wr $sys/net/ipv4/ipfrag_low_thresh 4194304
 wr $sys/net/ipv4/ip_forward 1
@@ -142,52 +122,41 @@ wr $sys/net/ipv4/tcp_fastopen 1
 wr $sys/net/ipv4/tcp_fin_timeout 10
 wr $sys/net/ipv4/tcp_frto 1
 wr $sys/net/ipv4/tcp_keepalive_time 3600
-wr $sys/net/ipv4/tcp_keepalive_probes 3
-wr $sys/net/ipv4/tcp_low_latency 1
+wr $sys/net/ipv4/tcp_keepalive_probes 5
+wr $sys/net/ipv4/tcp_low_latency 0
 wr $sys/net/ipv4/tcp_max_orphans 8192
 wr $sys/net/ipv4/tcp_max_syn_backlog 256 #fix
 wr $sys/net/ipv4/tcp_max_tw_buckets 8192
 wr $sys/net/ipv4/tcp_mem "8192 16384 20480"
-wr $sys/net/ipv4/tcp_moderate_rcvbuf 0
-wr $sys/net/ipv4/tcp_mtu_probing 0
+wr $sys/net/ipv4/tcp_moderate_rcvbuf 1
+wr $sys/net/ipv4/tcp_mtu_probing 1
 wr $sys/net/ipv4/tcp_no_metrics_save 1
 wr $sys/net/ipv4/tcp_reordering 3
-wr $sys/net/ipv4/tcp_retries1 3
-wr $sys/net/ipv4/tcp_retries2 7
+wr $sys/net/ipv4/tcp_retries1 5
+wr $sys/net/ipv4/tcp_retries2 10
 wr $sys/net/ipv4/tcp_rfc1337 0
 wr $sys/net/ipv4/tcp_sack 1
 wr $sys/net/ipv4/tcp_synack_retries 3
 wr $sys/net/ipv4/tcp_timestamps 0
 wr $sys/net/ipv4/tcp_tw_recycle 0
 wr $sys/net/ipv4/tcp_tw_reuse 0
-wr $sys/net/ipv4/tcp_window_scaling 0
+wr $sys/net/ipv4/tcp_window_scaling 1
 wr $sys/net/ipv4/tcp_rmem "65536 131072 1048576"
 wr $sys/net/ipv4/tcp_wmem "65536 131072 1048576"
 wr $sys/net/ipv4/udp_mem "65536 131072 1048576"
 wr $sys/net/ipv4/udp_wmem_min 65536
 wr $sys/net/ipv4/route/flush 1
-}
 
-vm_generic(){
-wr $sys/vm/highmem_is_dirtyable 1
-wr $sys/vm/min_free_kbytes 1024
+# vm_base
+wr $sys/vm/highmem_is_dirtyable 0
+wr $sys/vm/min_free_kbytes 8192
 wr $sys/vm/oom_kill_allocating_task 1
 wr $sys/vm/oom_dump_tasks 0
 wr $sys/vm/panic_on_oom 0
 wr $sys/vm/page-cluster 1
 wr $sys/vm/stat_interval 60
-}
 
-sysctl_apply(){
-fs_generic
-kernel_generic
-net_generic
-vm_generic
-}
-
-sysctl_apply
-
-unset sys fs kernel vm_generic net sysctl_apply
+unset sys
 }
 
 #===================================================#
@@ -199,65 +168,56 @@ unset sys fs kernel vm_generic net sysctl_apply
 M4(){
 
 iotweak(){
-
-w="wrl /sys/block/$1/queue"
-
-$w/max_sectors_kb $2
-$w/iostats $3 
-$w/add_random $4 
-$w/read_ahead_kb $5
-$w/nomerges $6
-$w/rotational $7
-$w/rq_affinity $8
-
+if [ -d /sys/block/$1/queue ]; then
+    w="wrl /sys/block/$1/queue"
+     if [ ! "$2" -gt "$(cat /sys/block/$1/queue/max_hw_sectors_kb)" ]; then
+          $w/max_sectors_kb $2
+     fi
+     $w/nr_request $3
+     $w/read_ahead_kb $4
+     $w/nomerges $5
+     $w/rq_affinity $6
+     $w/iostats $7
+     $w/add_random $8
+     $w/rotational $9
+fi
 }
 
-iotweak mmcblk0 256 0 0 0 0 0 1
-iotweak mmcblk1 256 0 0 0 0 0 1
-iotweak dm-0 127 0 0 0 0 0 1
+iotweak mmcblk0 512 512 32 2 2 0 0 0
+iotweak mmcblk1 128 32   32 2 0 0 0 0
+iotweak dm-0        512 512 32 2 2 0 0 0
+iotweak dm-1        512 512 32 2 2 0 0 0 
 
 for x in /sys/block/*; do
-
-w="wrl $x/queue/iosched"
-
-case $(read $queue/scheduler) in
-
-*cfqd*)
-
-wrl $x/scheduler cfq
-
-# cfq specific tuning
-
-$w/slice_idle 0
-$w/back_seek_max 0
-$w/back_seek_penalty 0
-$w/fifo_expire_async 5000
-$w/fifo_expire_sync 500
-$w/low_latency 0
-$w/target_latency 0
-$w/group_idle 0
-$w/slice_async 200
-$w/slice_async_rq 32
-$w/quantum 32
-$w/slice_sync 1000;;
-
-*deadline*)
-
-wrl $x/scheduler deadline
-
-# deadline specific tuning
-
-$w/fifo_batch 64
-$w/writes_starved 8
-$w/read_expire 100
-$w/write_expire 2000
-$w/front_merges 0;;
-
-*)
-
-search noop $queue/scheduler && wrl $queue/scheduler noop
-
-esac; done
+    queue="$x/queue"
+    w="wrl $queue/iosched"
+    case $(read $queue/scheduler) in
+        *cfq*)
+            wrl $queue/scheduler cfq
+            $w/slice_idle 0
+            $w/back_seek_max 0
+            $w/back_seek_penalty 0
+            $w/fifo_expire_async 5000
+            $w/fifo_expire_sync 500
+            $w/low_latency 0
+            $w/target_latency 0
+            $w/group_idle 0
+            $w/slice_async 200
+            $w/slice_async_rq 32
+            $w/quantum 32
+            $w/slice_sync 1000;;
+        *deadline*)
+            wrl $queue/scheduler deadline
+            $w/fifo_batch 16
+            $w/writes_starved 0
+            $w/read_expire 100
+            $w/write_expire 10000
+            $w/front_merges 1;;
+        *)
+            search noop $queue/scheduler &&\
+            wrl $queue/scheduler noop
+    esac
+done
 
 }
 
@@ -275,44 +235,49 @@ search(){ read $2 | grep $1 >> /dev/null; }
 # 1 (false) for "not found". Does not print.
 
 wr(){
-[ -e $1 ] && $(echo -e $2 > $1 || echo "$1 write error."); }
+[ -e $1 ] && $(echo -e $2 > $1 ||\
+echo "$1 write error.")
+}
 
 wrl(){
-[ -e $1 ] && chmod 666 $1 && echo $2 > $1 && chmod 444 $1; }
+[ -e $1 ] && chmod 666 $1 &&\
+echo $2 > $1 && chmod 444 $1
+}
 
-if [ $dumpE == 1 ]; then # initialize dump
+if [ $dumpE == 1 ]; then
+    dpath=/data/$scriptname
+    for x in $dpath*; do
+        [ -e $x ] && rm $x
+    done
+    dump="$dpath-$(date +%Y-%m-%d).txt"
 
-dpath=/data/$scriptname
+    wr(){
+    if [ -e $1 ]; then
+        echo -e "WR - A: $1 = $(cat $1)\nWR - B: $1 = $2\n" >> $dump
+    fi
+}
 
-for x in $dpath*; do
-[ -e $x ] && rm $x; done
-
-dump="$dpath-$(date +%Y-%m-%d).txt"
-
-wr(){
-if [ -e $1 ]; then
-echo -e "WR - A: $1 = $(cat $1)\nWR - B: $1 = $2\n" >> $dump; fi; }
-
-wrl(){
-if [ -e $1 ]; then
-echo -e "WRL - A: $1 = $(cat $1)\nWRL - B: $1 = $2\n" >> $dump;
-chmod 666 "$1"; fi; }
+    wrl(){
+    if [ -e $1 ]; then
+        echo -e "WRL - A: $1 = $(cat $1)\nWRL - B: $1 = $2\n" >> $dump;
+        chmod 666 "$1"
+    fi
+}
 
 fi # end dump
 
 marker="/data/$scriptname-last-run"
 
-if [ $dumpE == 0 ]; then touch $marker; echo $(date) > $marker; fi
+if [ $dumpE == 0 ]; then
+    touch $marker; echo $(date) > $marker
+fi
 unset marker
-
-# Get highest CPU core number
-kernel_max=$(cat /sys/devices/system/cpu/kernel_max)
 
 # Get RAM size in KB 
 msize=$(cat /proc/meminfo | grep "MemTotal" | awk '{ print $2 }')
 
 generic_opt
 
-unset generic_opt dumpE msize kernel_max apply dump dpath marker
+unset generic_opt dumpE msize apply dump dpath marker
 
 exit 0
