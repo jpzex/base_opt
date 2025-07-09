a74ad74b1154040ab1152c7622142094bf1f2b72
jpzex <joao.pedro.menezes1998@GMAIL.COM>
2025-07-09 18:50:13 -0300

    Update base_opt.sh to v1.1.1 (old)
    Old version, do not use.

--- base_opt.sh
+++ base_opt.sh
@@ -1,13 +1,15 @@
 # base_opt.sh
-# Version 1.1
-# 2022-05-27 @ 21:36 (UTC)
+# Version 1.1.1
+# 2022-05-26 @ 00:20 (UTC)
 # ID: RELEASE
-# Written by jpzex (XDA & Telegram)
+# Written by @jpzex (XDA & Telegram)
 # Use at your own risk, Busybox is required.
+
+#set -xv # debug
 
 ##### USER SET VARIABLES #####
 
-# Dump mode (0 or 1): log before and after for every value that would be applied.
+# Dump mode (0 or 1): log before and after for every value that is getting applied.
 dump=0
 
 # Dry run mode (0 or 1): do not change any value, just dump before and after if dump=1.
@@ -15,14 +17,15 @@ dryrun=0
 
 ##### DO NOT EDIT BELOW THIS LINE #####
 
-which busybox > /dev/null
+prep(){
 
-if [ ! $? == 0 ]; then
-    echo "No busybox found.";
-    exit 0;
-fi
+np=/dev/null
 
-alias_list="mountpoint awk echo grep chmod fstrim cat mount"
+which busybox > $np
+
+[ $? != 0 ] && echo "No busybox found, please install it first. If you just installed, a reboot may be necessary." && exit 1
+
+alias_list="mountpoint awk echo grep chmod fstrim cat mount uniq"
 
 for x in $alias_list; do
     alias $x="busybox $x";
@@ -30,59 +33,48 @@ done
 
 scriptname=base_opt
 
+}
+
 # Generic optimizations.
 # May not affect battery usage, just remove bottlenecks.
 
-generic_opt(){
+main_opt(){
 M1 # ext4 and f2fs mountpoints
 M2 # sysctl (generic)
-M4 # I/O
+M4 # I/O 
 }
 
 #===================================================#
 #===================================================#
 #===================================================#
 
-# Module 1: Adjust mount flags based on FS
+# Module 1: Set mount flags to decrease overhead and improve writing performance
 
 M1(){ 
 
-read /proc/mounts | grep " / ext4" > /dev/null && root=/
+if [ $dryrun == 0 ]; then
+    for x in $(cat /proc/mounts | cut -d ' ' -f 1,2,3 | \
+tr ' ' '&' | grep "/dev/" | grep -E 'ext4|f2fs'); do
 
-list="/system /data /cache /data/sdext2 $root"
+        mpts=( $(echo $x | tr '&' ' ') )
+        mountpoint -q ${mpts[1]} || continue
 
-for x in $list; do
-    [ -e $x ] && [ ! -L $x ] && mountpoint -q $x &&\
-    search " $x " /proc/mounts && list2="$x $list2"
-    devlist="$(grep /dev/block /proc/mounts | \
-    grep "$x " | awk '{ print $1 }') $devlist";
-done
+        case ${mpts[2]} in
+            ext4 )
+                flags=remount,noatime,nodiratime ;;
+            f2fs )
+                flags=remount,noatime,nodiratime,flush_merge ;;
+            * ) 
+                continue ;;
+        esac
 
-for x in $list; do
-    case $(grep " $x " /proc/mounts | awk '{ print $3 }') in
-        "ext4")
-            flags="discard barrier noatime nodiratime";;
-        "f2fs")
-            flags="active_logs=4 noatime nodiratime \
-            noacl nobarrier background_gc=on";;
-         *)
-            break;; 
-    esac
-    if [ $dryrun == 0 ]; then
-        for y in $flags; do
-            mount -o remount,$y $x
-        done
-    fi
-done
+        mount -o remount,$flags ${mpts[0]}
+        [ $(which fstrim) ] && fstrim ${mpts[1]} || \
+        echo "${mpts[1]} fstrim error."
+    done
+fi
 
-# fstrim all existing f2fs and ext4 partitions.
-
-[ $(which fstrim) ] && [ $dryrun == 0 ] &&\
-for x in $list2; do
-    [ -e $x ] && mountpoint -q $x && fstrim $x
-done
-
-unset list list2 devlist flags x root
+unset x mpts 
 
 }
 
@@ -96,25 +88,33 @@ M2(){
 
 sys=/proc/sys
 
-# fs_base
-wr $sys/fs/file-max 131072
-wr $sys/fs/inotify/max_queued_events 1048576
-#wr $sys/fs/inotify/max_user_instances 1024 # not mess
-wr $sys/fs/inotify/max_user_watches 1048576
+fs_base(){
+wr $sys/fs/aio-max-nr 131072 #65536 def
+wr $sys/fs/file-max 131072 #65536 sug
+wr $sys/fs/inode-max 524288 #262144 sug
+wr $sys/fs/nr_open 2097152 #1048576 def
+wr $sys/fs/inotify/max_queued_events 1048576 #16384 def
+#wr $sys/fs/inotify/max_user_instances 1024 # 128 def
+wr $sys/fs/inotify/max_user_watches 1048576 #8192 def
+}
 
-# kernel_base
+kernel_base(){
 wr $sys/kernel/ctrl-alt-del 0
-wr $sys/kernel/panic 10
+wr $sys/kernel/dmesg_restrict 1
+wr $sys/kernel/panic 30 # 60 sug
 wr $sys/kernel/panic_on_oops 1
+wr $sys/kernel/perf_cpu_time_max_percent 0 #def 25
+wr $sys/kernel/perf_event_mlock_kb 0 #def 516
 wr $sys/kernel/printk "0 0 0 0"
+}
 
-# net_base
-wr $sys/net/core/netdev_max_backlog 512
-wr $sys/net/core/rmem_default 524288
-wr $sys/net/core/rmem_max 4194304
-wr $sys/net/core/somaxconn 512
-wr $sys/net/core/wmem_default 524288
-wr $sys/net/core/wmem_max 4194304
+net_base(){
+wr $sys/net/core/netdev_max_backlog 16384 # sug
+wr $sys/net/core/rmem_default 1048576 # sug
+wr $sys/net/core/rmem_max 2097152 # manual
+wr $sys/net/core/somaxconn 8192 #8192 sug
+wr $sys/net/core/wmem_default 1048576 # mirr rmem
+wr $sys/net/core/wmem_max 2097152 # mirr rmem
 wr $sys/net/ipv4/ipfrag_high_thresh 8388608
 wr $sys/net/ipv4/ipfrag_low_thresh 4194304
 wr $sys/net/ipv4/ip_forward 1
@@ -128,15 +128,14 @@ wr $sys/net/ipv4/tcp_early_retrans 2
 wr $sys/net/ipv4/tcp_ecn 0
 wr $sys/net/ipv4/tcp_fack 1
 wr $sys/net/ipv4/tcp_fastopen 1
-wr $sys/net/ipv4/tcp_fin_timeout 10
+wr $sys/net/ipv4/tcp_fin_timeout 5 #nateware
 wr $sys/net/ipv4/tcp_frto 1
-wr $sys/net/ipv4/tcp_keepalive_time 3600
-wr $sys/net/ipv4/tcp_keepalive_probes 5
+wr $sys/net/ipv4/tcp_keepalive_time 1800 #900 sug
+wr $sys/net/ipv4/tcp_keepalive_probes 2
 wr $sys/net/ipv4/tcp_low_latency 0
 wr $sys/net/ipv4/tcp_max_orphans 8192
-wr $sys/net/ipv4/tcp_max_syn_backlog 256 #fix
-wr $sys/net/ipv4/tcp_max_tw_buckets 8192
-wr $sys/net/ipv4/tcp_mem "8192 16384 20480"
+wr $sys/net/ipv4/tcp_max_syn_backlog 8192 #nateware
+wr $sys/net/ipv4/tcp_max_tw_buckets 65536 #nateware
 wr $sys/net/ipv4/tcp_moderate_rcvbuf 1
 wr $sys/net/ipv4/tcp_mtu_probing 1
 wr $sys/net/ipv4/tcp_no_metrics_save 1
@@ -144,26 +143,37 @@ wr $sys/net/ipv4/tcp_reordering 3
 wr $sys/net/ipv4/tcp_retries1 5
 wr $sys/net/ipv4/tcp_retries2 10
 wr $sys/net/ipv4/tcp_rfc1337 0
-wr $sys/net/ipv4/tcp_sack 1
-wr $sys/net/ipv4/tcp_synack_retries 3
-wr $sys/net/ipv4/tcp_timestamps 0
+wr $sys/net/ipv4/tcp_sack 1 # sug 
+wr $sys/net/ipv4/tcp_slow_start_after_idle 0 #nateware
+wr $sys/net/ipv4/tcp_syn_retries 2
+wr $sys/net/ipv4/tcp_synack_retries 2
+wr $sys/net/ipv4/tcp_timestamps 1
 wr $sys/net/ipv4/tcp_tw_recycle 0
-wr $sys/net/ipv4/tcp_tw_reuse 0
+wr $sys/net/ipv4/tcp_tw_reuse 1 #nateware
 wr $sys/net/ipv4/tcp_window_scaling 1
-wr $sys/net/ipv4/tcp_rmem "65536 131072 1048576"
-wr $sys/net/ipv4/tcp_wmem "65536 131072 1048576"
-wr $sys/net/ipv4/udp_mem "65536 131072 1048576"
-wr $sys/net/ipv4/udp_wmem_min 65536
+wr $sys/net/ipv4/tcp_rmem "8192 1048576 2097152" #Cloudflare + nateware inspired
+wr $sys/net/ipv4/tcp_wmem "8192 1048576 2097152" #mirr rmem
+wr $sys/net/ipv4/udp_rmem_min 8192
+wr $sys/net/ipv4/udp_wmem_min 8192
 wr $sys/net/ipv4/route/flush 1
+}
 
-# vm_base
-wr $sys/vm/highmem_is_dirtyable 0
-wr $sys/vm/min_free_kbytes 8192
-wr $sys/vm/oom_kill_allocating_task 1
+vm_base(){
+wr $sys/vm/admin_reserve_kbytes 4096
+wr $sys/vm/block_dump 0
+wr $sys/vm/highmem_is_dirtyable 1
+wr $sys/vm/min_free_kbytes $(($msize/200)) # 0.5% of total RAM 
+wr $sys/vm/oom_kill_allocating_task 0
 wr $sys/vm/oom_dump_tasks 0
 wr $sys/vm/panic_on_oom 0
-wr $sys/vm/page-cluster 1
-wr $sys/vm/stat_interval 60
+wr $sys/vm/page-cluster 12
+wr $sys/vm/stat_interval 30
+}
+
+fs_base
+kernel_base
+net_base
+vm_base
 
 unset sys
 }
@@ -178,29 +188,37 @@ M4(){
 
 iotweak(){
 if [ -d /sys/block/$1/queue ]; then
-    w="wrl /sys/block/$1/queue"
+     w="wrl /sys/block/$1/queue"
      if [ ! "$2" -gt "$(cat /sys/block/$1/queue/max_hw_sectors_kb)" ]; then
-          $w/max_sectors_kb $2
-     fi
-     $w/nr_request $3
-     $w/read_ahead_kb $4
-     $w/nomerges $5
-     $w/rq_affinity $6
-     $w/iostats $7
-     $w/add_random $8
-     $w/rotational $9
+          $w/max_sectors_kb $2; fi  # 1
+     $w/nr_request $3                     # 2
+     $w/read_ahead_kb $4             # 3
+     $w/nomerges $5                      # 4
+     $w/rq_affinity $6                      # 5
+     $w/iostats 0                              # decrease overhead
+     $w/add_random 1                   # help create randomness
+     $w/rotational 0                         # all flash storage
 fi
 }
 
-iotweak mmcblk0 512 512 32 2 2 0 0 0
-iotweak mmcblk1 128 32   32 2 0 0 0 0
-iotweak dm-0        512 512 32 2 2 0 0 0
-iotweak dm-1        512 512 32 2 2 0 0 0 
+# usually emmc
+iotweak mmcblk0 512 1024 512 0 2
+
+# usually sd card
+iotweak mmcblk1 512 1024 512 0 2
+
+# other dm partitions
+for x in $(seq 0 5); do
+    iotweak dm-$x 512 1024 512 0 2
+done
+
+# the more you know...
 
 for x in /sys/block/*; do
     queue="$x/queue"
     w="wrl $queue/iosched"
     case $(read $queue/scheduler) in
+
         *cfq*)
             wrl $queue/scheduler cfq
             $w/slice_idle 0
@@ -214,17 +232,18 @@ for x in /sys/block/*; do
             $w/slice_async 200
             $w/slice_async_rq 32
             $w/quantum 32
-            $w/slice_sync 1000;;
-        *deadline*)
+            $w/slice_sync 1000
+            break;;
+
+         *deadline*)
             wrl $queue/scheduler deadline
             $w/fifo_batch 16
             $w/writes_starved 0
-            $w/read_expire 100
+            $w/read_expire 1000
             $w/write_expire 10000
-            $w/front_merges 1;;
-        *)
-            search noop $queue/scheduler &&\
-            wrl $queue/scheduler noop
+            $w/front_merges 0
+             break;;
+
     esac
 done
 
@@ -234,12 +253,14 @@ done
 #===================================================#
 #===================================================#
 
+vars(){
+
 # Get RAM size in KB 
 msize=$(cat /proc/meminfo | grep "MemTotal" | awk '{ print $2 }')
 
 read(){ [ -e $1 ] && cat $1; }
 
-search(){ read $2 | grep $1 >> /dev/null; }
+search(){ read $2 | grep $1 > $np ; }
 
 # search <string> <file>
 # searches for string in file if it exists and returns
@@ -279,7 +300,7 @@ if [ $dump == 1 ]; then
         [ -e $x ] && rm $x
     done
     dpath="$dpath-$(date +%Y-%m-%d).txt"
-    dumpinfo="The dump file is located in: $dpath. The values $have been applied, according to the config on the start of the script."
+    echo "The dump file is located in: $dpath. The values $have been applied, according to the config on the start of the script."
 
     wr(){
     if [ $dump == 1 ]; then
@@ -303,10 +324,9 @@ fi # end dump
 
 #=DUMP=AND=DRY=RUN=END==============================#
 
-generic_opt
+} # end vars
 
-echo $dumpinfo
+prep && vars && main_opt
+#if [ -z $dumpinfo ]; then echo $dumpinfo; fi
 
-unset generic_opt msize apply dump dryrun dpath wr wrl read search dumpinfo have
-
-exit 0
+unset main_opt scriptname alias_list msize apply dump dryrun dpath wr wrl read search dumpinfo have np
